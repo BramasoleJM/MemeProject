79701de0dc42c898009d20d17ca06b18b5adf56f
BramasoleJM <bramasolejm030206@gmail.com>
2022-07-17 12:22:02 +0800

    player movement
    update player idle, run and jump animation

--- Project Meme/Assets/Scripts/PlayerMovement.cs
+++ Project Meme/Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,20 +7,54 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
     private Rigidbody2D body;
+    private Animator anim;
+    private bool grounded;
+    
     // Start is called before the first frame update
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
 
-        if (Input.GetKey(KeyCode.Space))
+        //flap side when move
+        if (horizontalInput > 0.01f)
         {
-            body.velocity = new Vector2(body.velocity.x, speed);
+            transform.localScale = Vector3.one;
+        }
+        else if (horizontalInput < -0.01f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        
+        //jump
+        if (Input.GetKey(KeyCode.Space) && grounded)
+        {
+            Jump();
+        }
+        
+        //Set animator parameters
+        anim.SetBool("run", horizontalInput != 0);
+        anim.SetBool("grounded", grounded);
+    }
+
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speed);
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            grounded = true;
         }
     }
 }
