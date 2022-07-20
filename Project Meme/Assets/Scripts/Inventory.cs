ada878cc2bb805f6cc3bc4472c0c8897cc0a7812
zhi-dian <2919261425@qq.com>
2022-07-21 00:36:13 +0800

    on inventory
    add functions on change select item. (little process)
    
    need to change logic when using items.

--- Project Meme/Assets/Scripts/Inventory.cs
+++ Project Meme/Assets/Scripts/Inventory.cs
@@ -6,6 +6,8 @@ public class Inventory : MonoBehaviour
 {
     public bool[] isFull;
     public GameObject[] slots;
+
+    public int hold = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +17,21 @@ public class Inventory : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            hold = 0;
+        }else if (Input.GetKey(KeyCode.Alpha2))
+        {
+            hold = 1;
+        }
+        else if(Input.GetKey(KeyCode.Alpha3))
+        {
+            hold = 2;
+        }else if (Input.GetKey(KeyCode.Alpha4))
+        {
+            hold = 4;
+        }
+        
         
     }
 }
--- Project Meme/Assets/Scripts/Pickup.cs
+++ Project Meme/Assets/Scripts/Pickup.cs
@@ -21,15 +21,22 @@ public class Pickup : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.E) && triggered)
         {
+            bool find = false;
             for (int i = 0; i < inventory.slots.Length; i++)
             {
                 if (inventory.isFull[i] == false)
                 {
+                    find = true;
                     inventory.isFull[i] = true;
                     Instantiate(itemButton, inventory.slots[i].transform, false);
                     Destroy(gameObject);
                     break;
                 }
+            }
+
+            if (!find)
+            {
+                
             }
         }
     }
